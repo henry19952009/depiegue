d6ed009bdd03e02f14391cf76698e2136fedb076
henry19952009 <henry19952009@hotmail.com>
2021-12-10 08:38:45 -0500

    nuevo 2.0

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const experess = require('express');
+const express = require('express');
 const path = require('path');
-const app = experess();
+const app = express();
 
 app.use(express.static('./dist/E-shopAppTeamBackEnd'));
 
